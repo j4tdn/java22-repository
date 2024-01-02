c55eb4cab65f669c3e76a8e076d0567a9c825353
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-01-02 09:50:37 +0700

    fix ex1 02.01.2024

--- lesson02-hdoan/src/homework/Ex01.java
+++ lesson02-hdoan/src/homework/Ex01.java
@@ -2,9 +2,10 @@ package homework;
 
 import java.util.Scanner;
 
-/* Bài 1: Viết chương trình(hàm) kiểm tra số nguyên N có phải là bội của 2 hay không 
- * Yêu cầu: N là số nguyên dương được nhập từ bàn phím, bắt buộc nhập lại nếu N không hợp lệ (tối đa 5 lần) 
- * VD: Nhập N = abc → báo lỗi, yêu cầu nhập lại = 4 → true = 6 → true = 7 → false */
+/* 	Bài 1: Viết chương trình(hàm) kiểm tra số nguyên N có phải là bội của 2 hay không 
+	Yêu cầu: N là số nguyên dương được nhập từ bàn phím, bắt buộc nhập lại nếu N không hợp lệ (tối đa 5 lần) 
+	VD: Nhập N = abc → báo lỗi, yêu cầu nhập lại = 4 → true = 6 → true = 7 → false 
+*/
 public class Ex01 {
 	public static boolean isMultipleOf2(String n) {
 		if (!n.matches("\\d+")) { // nếu chuỗi n không chỉ chứa các chữ số, thì hàm sẽ trả về false
@@ -17,7 +18,7 @@ public class Ex01 {
 		Scanner ip = new Scanner(System.in);
 		int count = 0;
 		while (count < 5) {
-			System.out.print("Nhập số nguyên dương N: ");
+			System.out.print("Nhập N: ");
 			String input = ip.nextLine();
 			if (isMultipleOf2(input)) {
 				System.out.println(input + " là bội của 2");
@@ -25,7 +26,8 @@ public class Ex01 {
 			} else if (!input.matches("\\d+")) { //matches() so sánh chuỗi n với biểu thức chính quy "\d+"
 				System.out.println("không hợp lệ. Nhập lại: "); //\d = một chữ số, + là lượng tử hóa = một hoặc nhiều
 			} else {
-				System.out.println("không phải là bội của 2. Nhập lại: ");
+				System.out.print("không phải là bội của 2");
+				return;
 			}
 			count++;
 		}
