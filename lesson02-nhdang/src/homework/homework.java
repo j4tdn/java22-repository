ab67e846e7d902c9b1c052ab4614edbb04c62890
MiiNorin <hodangdxb@gmail.com>
2024-01-02 17:11:04 +0700

    lesson02-exercises 2/1/2023

--- lesson02-nhdang/src/homework/homework.java
+++ lesson02-nhdang/src/homework/homework.java
@@ -1,56 +1,64 @@
-package video_array;
+package homework;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
-public class video_array {
+public class homework {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+
 		int c;
 		do {
-			System.out.println("Chọn chức năng");
-			System.out.println("1. Case 1");
-			System.out.println("2. Case 2");
-			System.out.println("3. Case 3");
-			System.out.println("4. Case 4");
-			System.out.println("5. Case 5");
-			System.out.println("6. Case 6");
-			System.out.println("7. Case 7");
-			System.out.println("8. Case 8");
-			System.out.println("9. Case 9");
-			System.out.println("10. Case 10");
+			System.out.println("=======================");
+			System.out.println("1. Kiểm tra số nguyên N có phải là bội của 2 hay không");
+			System.out.println("2. Kiểm tra số nguyên N có phải là lũy thừa của 2 hay không");
+			System.out.println("3. Tìm giai thừa của một số nguyên dương N");
+			System.out.println("4. Tính tổng giải thừa");
+			System.out.println("5. Kiểm tra N có phải là số đối xứng");
+			System.out.println("6. Tìm số nguyên lớn, nhỏ nhất trong 3 chữ số");
+			System.out.println("7. Chuyển cơ số nhị phân");
+			System.out.println("8. Kiểm tra số nguyên tố");
+			System.out.println("9. Tìm số nguyên tố thứ 200");
+			System.out.println("10. In ra 5 số nguyên ngẫu nhiên từ 20 đến 30");
+			System.out.println("11. Kết thúc chương trình");
+			System.out.println("=======================");
 			c = sc.nextInt();
 			switch (c) {
 			case 1:
-				case1();
+
+				ex1();
 				break;
 			case 2:
-				case2();
+				ex2();
 				break;
 			case 3:
-				case3();
+				ex3();
 				break;
 			case 4:
-				case4();
+				ex4();
 				break;
 			case 5:
+				ex5();
 				break;
 			case 6:
-				case6();
+				ex6();
 				break;
 			case 7:
+				ex7();
 				break;
 			case 8:
+				ex8();
 				break;
 			case 9:
+				ex9();
 				break;
 			case 10:
 				break;
 			case 11: 
-				System.out.println("Ket thuc");
+				System.out.println("JAVA BKIT 22");
 				break;
 			default:
 				System.out.println("Khong ho tro");
@@ -60,7 +68,11 @@ public class video_array {
 	}
 
 
-	public static void case1() {
+
+
+
+	public static void ex1() {
+		System.out.println("=======================");
 		int x;
 		int count = 0;
 		Scanner sc = new Scanner (System.in);
@@ -90,7 +102,9 @@ public class video_array {
 			System.out.println("Qua so lan thu");
 		}
 	}
-	public static void case2() {
+	
+	public static void ex2() {
+		System.out.println("=======================");
 		int x;
 		int count = 0;
 		Scanner sc = new Scanner (System.in);
@@ -100,12 +114,12 @@ public class video_array {
 				String a = sc.nextLine();
 				x = Integer.parseInt(a);
 				if ((x!=0)&&((x&((x-1))))==0) {
-					System.out.println("->True");
+					System.out.println("So nguyen " + x + "la luy thua cua 2");
 				} 
 				else {					
 					count=count +1;
 					if (count <= 5) {
-						System.out.println(x+ "->False");
+						System.out.println("So nguyen " + x + " khong phai la luy thua cua 2");
 					}
 				}
 			} catch (NumberFormatException e) {
@@ -120,12 +134,12 @@ public class video_array {
 			System.out.println("Qua so lan thu");
 		}
 	}
-	public static void case3() {
+	
+	public static void ex3() {
+		System.out.println("=======================");
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Nhap so nguyen N");
 		int sum = 1;
-
-
 		try {
 			String a = sc.nextLine();
 			int x = Integer.parseInt(a);
@@ -142,7 +156,9 @@ public class video_array {
 			System.out.println("Sai dinh dang");
 		}
 	}
-	public static void case4() {
+	
+	public static void ex4() {
+		System.out.println("=======================");
 		Random rd = new Random();
 		long sum1 = 1;
 		long sum2 = 2;
@@ -175,18 +191,53 @@ public class video_array {
 
 		System.out.println("Tong giai thua cua 4 so random trong mang [10, 20]: " + sum);
 	}
-	public static void case5() {
-
+	
+	public static void ex5() {
+		System.out.println("=======================");
+		Scanner sc = new Scanner(System.in);
+		boolean isInt;
+		do {
+			try {  
+				System.out.println("Nhap N");
+				String x = sc.nextLine();
+				int a = Integer.parseInt(x);
+				int temp = a;
+				int sum = 0;
+				if (a<10) {
+					System.out.println("N phai co it nhat 2 chu so");
+					isInt=false;
+				}
+				else {
+					while(temp!=0) {
+						sum = sum*10+temp%10;
+						temp = temp/10;
+					}
+					if(sum==a) {
+						System.out.println("Day la so doi xung");
+					} else {
+						System.out.println("Day khong phai la so doi xung");
+					}
+					break;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Sai dinh dang");
+				isInt = false;
+			}
+		} while (isInt = true);
 	}
-	public static void case6() {
+	
+	public static void ex6() {
+		System.out.println("=======================");
 		Scanner sc = new Scanner(System.in);
 		int a=0;
 		int b=0;
 		int c=0;
 		int Max;
+		int Min;
 		boolean isInt;
 		do {
-			try {  System.out.println("Nhap a");
+			try {  
+			System.out.println("Nhap a");
 			String x = sc.nextLine();
 			a = Integer.parseInt(x);
 			if (a<0||a>20) {
@@ -202,7 +253,8 @@ public class video_array {
 			}
 		} while (isInt = true);
 		do {
-			try {  System.out.println("Nhap b");
+			try {  
+			System.out.println("Nhap b");
 			String y = sc.nextLine();
 			b = Integer.parseInt(y);
 			if (b<0||b>20) {
@@ -218,7 +270,8 @@ public class video_array {
 			}
 		} while (isInt = true);
 		do {
-			try {  System.out.println("Nhap c");
+			try {  
+			System.out.println("Nhap c");
 			String z = sc.nextLine();
 			c = Integer.parseInt(z);
 			if (c<0||c>20) {
@@ -234,12 +287,75 @@ public class video_array {
 			}
 		} while (isInt = true);
 		Max = a;
+		Min = b;
 		if (b>Max) {
 			Max = b;
+			Min = a;
 		}
 		if (c>Max) {
 			Max = c;
+		} else {
+			if(c<Min) {
+				Min=c;
+			}
 		}
-		System.out.println("Max la: " + Max);
+		System.out.println("So lon nhat la: " + Max);
+		System.out.println("So nho nhat la: " + Min);
+	}
+	
+	public static void ex7() {
+		System.out.println("=======================");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap so can kiem tra");
+		int n = sc.nextInt();
+		String binary = "";
+		while (n>0) {
+			binary = n%2+binary;
+			n = n/2;
+		}
+		System.out.println("He nhi phan cua " + n + " la" + binary);
+	}
+	
+	public static void ex8() {
+		System.out.println("=======================");
+		Scanner sc = new Scanner(System.in);
+		boolean isInt;
+		do {
+			try {  
+				System.out.println("Nhap N");
+				String x = sc.nextLine();
+				int a = Integer.parseInt(x);
+				int count = 0;
+				if(a==0&&a==1) {
+					System.out.println(a + " khong phai la so nguyen to");
+				}
+				
+				for (int i = 1; i<=a; i++) {
+					if(a%i==0) {
+						count = count+1;
+					}
+				}
+				if (count==2) {
+					System.out.println(a + " la so nguyen to");
+					break;
+				} else {
+					System.out.println(a + " khong phai la so nguyen to");
+					break;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Sai dinh dang");
+				isInt = false;
+			}
+		} while (isInt = true);
+
+	}
+	
+	public static void ex9() {
+		System.out.println("=======================");
+		long count = 0;
+		long n = 1;
+		while (count < n) {
+			
+		}
 	}
 }
