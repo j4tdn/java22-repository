a63dd61c2712f84caae93cae44c3ad21af529578
MiiNorin <124641240+MiiNorin@users.noreply.github.com>
2024-01-02 19:09:45 +0700

    Ex10

--- lesson02-nhdang/src/homework/Ex10.java
+++ lesson02-nhdang/src/homework/Ex10.java
@@ -4,34 +4,5 @@ import java.util.Scanner;
 
 public class Ex10 {
 	public static void main(String[] args) {
-		System.out.println("1. Kiểm tra số nguyên N có phải là bội của 2 hay không");
-		int x;
-		int count = 0;
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Nhap so nguyen N");
-		do {
-			try {
-				String a = sc.nextLine();
-				x = Integer.parseInt(a);
-				if (x%2==0 && x>0) {
-					System.out.println(x + "->True");
-				}
-				else {					
-					count=count +1;
-					if (count <= 5) {
-						System.out.println(x+ "->False");
-					}
-				}
-			} catch (NumberFormatException e) {
-				count = count + 1;
-				if(count<5) {
-					System.out.println("Sai dinh dang");
-				}
-			}
-		} while (count<5);
-
-		if (count==5) {
-			System.out.println("Qua so lan thu");
-		}
-	}
+	
 }
