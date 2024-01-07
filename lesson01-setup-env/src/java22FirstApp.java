a7927627a94b9339f201f4d44285b8537731495c
viet040702 <baanhle35@gmail.com>
2024-01-07 18:24:20 +0700

    Revert "lesson02-data-structure lesson03-opp 1.1.2024"
    This reverts commit f3d97295ad1bbddb9af7dffea1102e20174e30bd.

--- lesson01-setup-env/src/java22FirstApp.java
+++
@@ -1,6 +0,0 @@
-public class java22FirstApp {
-    public static void main(String[] args) {
-        System.out.println("== Hello xin chao cac ban ==");
-        System.out.println("today hoom nay toi buon");
-    }
-}
