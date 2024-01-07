28ea4cfcd456657ba82781dfc8c358d0c7fd5e2f
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-01-07 16:47:25 +0700

    lesson03-oop 07-01-2024

---
+++ lesson03-oop/src/view/Ex05StaticNonStatic_LoadingTime.java
@@ -0,0 +1,59 @@
+package view;
+
+public class Ex05StaticNonStatic_LoadingTime {
+
+	/*
+	  Attribute, Method
+	  
+	  + Static: Class, Object(warning)
+	  
+	  + Non-Static: Object
+	  
+	  Compile, Runtime
+	   
+	  Runtime(
+	  Static(khởi tạo ra cho class khi running, load chương trình) --> 
+	  Non-Static(chỉ thật sự khởi tạo khi khởi tạo đối tượng)
+	  
+	 */
+	//khởi tạo khi chạy chương trình lên
+	private static String staticVariable;
+	
+	//Thật sự được khởi tạo khi tạo ra 1 đối tượng từ class hiện tại
+	private String nonStaticVariable;
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+	private static void staticMethod() {
+		System.out.println("PRINT - STATIC METHOD");
+	
+		staticVariable = "static";
+		
+		// error: ko thể gọi biến, hàm non-static trong hàm static
+		// vì hàm static khởi tạo trước, giả sử lấy class gọi hàm static
+		// lúc đó biến, hàm static được gọi bên trong ko biết của đối tượng nào --> lỗi
+		
+		// nonStaticVariable = "none";
+		// nonStaticMethod();
+	}
+	
+	//Thật sự được khởi tạo khi tạo ra 1 đối tượng từ class hiện tại
+	private void nonStaticMethod() {
+		System.out.println("PRINT - NON STATIC METHOD");
+		
+		nonStaticVariable = "none";
+		
+		// để gọi được hàm non static, thì phải có 1 đối tượng gọi hàm
+		// lúc đối tượng gọi hàm, trước đó class đã khởi tạo rồi
+		// --> hàm, biến static đã có rồi
+		// --> gọi hàm, biến static bên trong non-static
+		
+		staticVariable = "static";
+		staticMethod();
+		
+	}
+
+}
