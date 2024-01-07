af1ed7bac946d0effcd008252c15bf93d1664d48
nambui37 <nambui37x11@gmail.com>
2024-01-07 07:49:48 +0700

    commit

---
+++ lesson02_ducnam/src/lesson02_5_10/Ex10.java
@@ -0,0 +1,36 @@
+package lesson02_5_10;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		int[] numbers = new int[5];
+		
+	  Random rd = new Random();
+	  int a = rd.nextInt(20, 30);
+	  numbers[0] = a;
+	  
+	  for(int i = 1 ; i <= 4; i++) {
+		  a = rd.nextInt(20, 31);
+		  if(checkExists(numbers, a)) {
+			  i--;
+		  } else {
+			  numbers[i] = a;
+		  }
+	  }
+	 
+	  
+		System.out.println("số nguyên ngẫu nhiên");
+        System.out.println(Arrays.toString(numbers));
+	}
+	
+	public static boolean checkExists(int[] arr, int value) {
+		for(int i=0; i< arr.length; i++) {
+			if(arr[i] == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
--- lesson03_oop/src/view/Ex01BasicCarDemo.java
+++ lesson03_oop/src/view/Ex01BasicCarDemo.java
@@ -1,22 +1,29 @@
 package view;
 
-
 import bean.Car;
 
 public class Ex01BasicCarDemo {
 	
-     public static void main(String[] args) {
-	// class Car (id ,odekl, 
-    	 
-    	 // tạo ra 3 cái đối tượng cho dũe liệu class car 
-    	 Car c1 = new Car("1", "MG5", "Yellow", 800d);
-    	 Car c2 = new Car("2", "Raize", "Yellow", 540d);
-    	 Car c3 = new Car("3", "Vf8", "Yellow", 720d);
-    
-    	 
-         System.out.println("c1 --> " + c1);
-          System.out.println("c2 --> " + c2);
-          System.out.println("c3 --> " + c3);
-     
-     }  
+	public static void main(String[] args) {
+		// class Car(id, model, color, salesPrice)
+		
+		// tạo ra 3 đối tượng cho class Car
+		
+		Car c1 = new Car("1", "MG5", "Yellow", 800d); // H1
+		Car c2 = new Car("2", "Raize", "Orange", 540d); // H2
+		Car c3 = new Car("3", "VF8", "Blue", 720d); // H3
+		
+		// cập nhật thông tin
+		// c2.salesPrice = 560d;
+		c2.setSalesPrice(560d);
+		
+		// lấy thông tin
+		// System.out.println("C3 Model --> " + c3.model);
+		System.out.println("C3 Model --> " + c3.getModel());
+		
+		System.out.println("c1 --> " + c1);
+		System.out.println("c2 --> " + c2);
+		System.out.println("c3 --> " + c3);
+	}
+	
 }
