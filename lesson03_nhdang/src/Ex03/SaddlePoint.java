b23fe9184fd7c78cb87f879ebeae1451ecf80325
MiiNorin <hodangdxb@gmail.com>
2024-01-10 18:53:12 +0700

    lesson03-exercises 10/1/2024

--- lesson03_nhdang/src/Ex03/SaddlePoint.java
+++ lesson03_nhdang/src/Ex03/SaddlePoint.java
@@ -4,12 +4,11 @@ package Ex03;
 import java.util.Random;
 import java.util.Scanner;
 
-public class YenNgua {
+public class SaddlePoint {
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner (System.in);
 		Random rd = new Random();
-		System.out.println("Nhập kích thước mảng");
+		System.out.println("Nhập kích thước mảng: ");
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		int arr[][] = new int[m][n];
@@ -25,35 +24,28 @@ public class YenNgua {
 			}
 			System.out.println();
 		}
-        int yenNgua = 0;
-        int max;
-		int min;
-		for (int i = 0; i<m; i++) {
-			int c=0;
+		for (int i = 0; i < arr.length; i++) {
+			int min;
+		    boolean saddlePoint = true; 
 			min = arr[i][0];
-			for (int j = 0; j<n; j++) {
-				if (min>arr[i][j]) {
+			int colIndex = 0;
+
+			for (int j = 1; j < arr[i].length; j++) {
+				if(arr[i][j] < min) {				
 					min = arr[i][j];
-				}
-			
-			}
-			max = arr[0][c];
-			for(int k=0;k<n;k++) {
-				if(max<arr[i][k]) {
-					max=arr[k][c];
+					colIndex = j;
 				}
 			}
-			if(min==max) {
-				yenNgua=min;
+			for (int j = 0; j < arr.length; j++){
+				if(arr[j][colIndex] > min){
+					saddlePoint = false;
+					break;
+				}
 			}
-//			if (min>arr[i][j]) {
-//				for (int j = 0; j<n; j++) {
-//				    if (min<arr[m][n]) {
-//				    	yenNgua=min;
-//				    }
-//				}
-//			}
-		}
-		System.out.println("Phần tử yên ngựa: " + yenNgua);
+			if(saddlePoint){
+				System.out.println("Phần tử yên ngựa : "+min);
+			}
+		} 
+
 	}
 }
