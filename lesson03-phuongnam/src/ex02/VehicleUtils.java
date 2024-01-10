f208d221756ab13b42ad673c71e102a9c511446c
Namnguyen265 <phuongnam260500@gmail.com>
2024-01-10 11:39:01 +0700

    lesson03-phuongnam 10.1.2024

---
+++ lesson03-phuongnam/src/ex02/VehicleUtils.java
@@ -0,0 +1,54 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class VehicleUtils {
+
+	static public void nhapXe(Vehicle car) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập mã xe: ");
+		car.setId(sc.nextLine());
+
+		System.out.print("Nhập tên chủ xe: ");
+		car.setOwner(sc.nextLine());
+
+		System.out.print("Nhập tên loại xe: ");
+		car.setType(sc.nextLine());
+
+		System.out.print("Nhập dung tích xe: ");
+		car.setCapacity(sc.nextInt());
+		sc.nextLine();
+
+		System.out.print("Nhập trị giá xe: ");
+		car.setCost(sc.nextDouble());
+		sc.nextLine();
+
+	}
+
+	public static void Print(Vehicle[] car) {
+		int n = car.length;
+		System.out.println(" Chủ sở hữu " + "    " + " Loại xe " + "    " + " Dung tích " + "    " + " Trị giá "
+				+ "    " + " Thuế phải nộp ");
+		System.out.println(
+				"==================================================================================================================");
+
+		for (int i = 0; i < n; i++) {
+			if (car[i].getCapacity() < 100) {
+				System.out.println("      " + car[i].getOwner() + "      " + car[i].getType() + "    "
+						+ car[i].getCapacity() + "    " + car[i].getCost() + "    " + car[i].getCost() * 0.01);
+			}
+			if (car[i].getCapacity() >= 100 && (car[i].getCapacity() <= 200)) {
+				System.out.println("      " + car[i].getOwner() + "      " + car[i].getType() + "    "
+						+ car[i].getCapacity() + "    " + car[i].getCost() + "    " + car[i].getCost() * 0.03);
+
+			}
+			if (car[i].getCapacity() > 200) {
+				System.out.println("      " + car[i].getOwner() + "      " + car[i].getType() + "    "
+						+ car[i].getCapacity() + "    " + car[i].getCost() + "    " + car[i].getCost() * 0.05);
+
+			}
+
+		}
+	}
+
+}
