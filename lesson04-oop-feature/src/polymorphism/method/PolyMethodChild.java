deb70dea1482f75225559501ad58f6893d327ff1
unknown <anhlethivan12369@gmail.com>
2024-01-13 07:59:48 +0700

    lesson04-ws

--- lesson04-oop-feature/src/polymorphism/method/PolyMethodChild.java
+++ lesson04-oop-feature/src/polymorphism/method/PolyMethodChild.java
@@ -7,4 +7,8 @@ public class PolyMethodChild extends PolyMethodParent{
 	void log() {
 		System.out.println("CHild---> LOG 2...");
 	}
+	
+	// 1. co the override thuoc tinh(static/non-static) duoc khong
+	// 2. co the override ham static duoc khong
+	//--> cho vi du
 }
