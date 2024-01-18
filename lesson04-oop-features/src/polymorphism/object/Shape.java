747f53bc1e260c681b916b86a8c3b0a9fd341253
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-01-18 21:04:23 +0700

    lesson04-oop-features 18.01.2024

---
+++ lesson04-oop-features/src/polymorphism/object/Shape.java
@@ -0,0 +1,15 @@
+package polymorphism.object;
+
+public class Shape {
+	
+	void paint() {
+		// chưa biết sẽ paint gì
+		System.out.println("Shape paint --> unknown ...");
+	}
+	
+	void calcS() {
+		// chưa biết sẽ tính diện tích như nào
+		System.out.println("Shape calcS --> unknown ...");
+	}
+	
+}
---
+++ lesson04-oop-features/src/polymorphism/object/Square.java
@@ -0,0 +1,19 @@
+package polymorphism.object;
+
+public class Square extends Shape {
+	
+	@Override
+	void paint() {
+		System.out.println("Square --> paint ...");
+	}
+	
+	@Override
+	void calcS() {
+		System.out.println("Square --> calS ...");
+	}
+	
+	void setBackground() {
+		System.out.println("Square --> setBackground ...");
+	}
+	
+}
