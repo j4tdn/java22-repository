9f94be43a0b1049727c9b21a7b189fe923bd5f9a
nambui37 <nambui37x11@gmail.com>
2024-01-19 08:22:20 +0700

    19-1-2023

--- lesson04-oop-features/src/encapsulation/packege_a/TestA2.java
+++ lesson04-oop-features/src/encapsulation/packege_a/TestA2.java
@@ -1,5 +1,12 @@
-package encapsulation.packege_a;
+package encapsulation.package_a;
 
 public class TestA2 {
+	public static void main(String[] args) {
+		//Sử dụng các class trong cùng package, ko cần phải import
+		//khác package --> Phải import
+		TestA1.methodTestA1();
+		TestA1.methodTestA1NoDeclare();
+		TestA1.methodTestA1Protected();
+	}
 
 }
