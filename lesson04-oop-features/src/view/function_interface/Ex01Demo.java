3379624078fadb057992487b99fe4b99a1c575a7
hminh <fbxl333@gmail.com>
2024-03-15 08:37:24 +0700

    lesson04-oop-features 15.03.2024

---
+++ lesson04-oop-features/src/view/function_interface/Ex01Demo.java
@@ -0,0 +1,19 @@
+package view.function_interface;
+
+public class Ex01Demo {
+	
+	/*
+	 * Functional Interface
+	 * Là interface có duy nhất một hàm trừu tượng
+	 * Ví dụ
+	 * interface Test01  {
+	 * 		void hello();
+	 * } --> là functional interface
+	 * 
+	 * interface Test02 {
+	 * 		void method01(int x);
+	 * 		int method02();
+	 * } --> ko phải
+	 */
+
+}
