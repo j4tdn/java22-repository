b431d2ec9f722ca82bdc7f4e3d0a8226c422ce9b
AHNCHOW <minhchau858301@gmail.com>
2024-04-18 17:17:21 +0700

    lesson08-nguyenanhchau 17.04.2024

--- lesson08-nguyenanhchau/src/Exercise08/test.java
+++ lesson08-nguyenanhchau/src/Exercise08/test.java
@@ -2,16 +2,16 @@ package Exercise08;
 
 public class test {
 	public static void main(String[] args) {
-		String S = "aaabaaabbaaaaa";
+		String S = "aaabaaabbaaaasfef";
 		int maxLength = 1;
 		int minLength = Integer.MAX_VALUE;
 
 		int count = 1;
 		for (int i = 1; i < S.length(); i++) {
 			if (S.charAt(i) == S.charAt(i - 1)) {
-				count++;
+				  count++;
 
-			}
+			}  
 
 		}
 
