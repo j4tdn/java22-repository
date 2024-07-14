056417dd45f733ef8d481753973d5f03c9771802
thanhminh <minh97ht@gmail.com>
2024-07-14 15:52:36 +0700

    lesson11-test 07.07.24

--- lesson11-thanhminh-test/src/model/DataModel.java
+++ lesson11-thanhminh-test/src/model/DataModel.java
@@ -13,13 +13,14 @@ public class DataModel {
 	}
     public static List<Transaction> getTransactions() {
 		Trader raoul = new Trader("Raoul", "Cambridge");
+		Trader raoul2 = new Trader("Raoul", "Cambridge");
 		Trader mario = new Trader("Mario", "Milan");
 		Trader alan = new Trader("Alan", "Cambridge");
 		Trader brian = new Trader("Brian", "Cambridge");
 		List<Transaction> transactions = Arrays.asList(
 			new Transaction(brian, 2011, 300),
 			new Transaction(raoul, 2012, 1000), 
-			new Transaction(raoul, 2011, 400),
+			new Transaction(raoul2, 2011, 400),
 			new Transaction(mario, 2012, 710), 
 			new Transaction(mario, 2012, 700), 
 			new Transaction(alan, 2012, 950)
---
+++ lesson11-thanhminh-test/src/utils/TransactionUtils.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class TransactionUtils {
+	private TransactionUtils() {
+		
+	}
+	
+
+}
