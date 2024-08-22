dac94e222620764bc03a25e34115a47e22338991
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-08-22 19:53:52 +0700

    lesson15-qphan 22.08.2024

---
+++ lesson15-qphan/src/view/WhAllocationDemo.java
@@ -0,0 +1,15 @@
+package view;
+
+public class WhAllocationDemo {
+	
+	public static void main(String[] args) {
+		
+		// use data from DataModel
+		
+		// do calculation
+		
+		// print result step by step
+		
+	}
+	
+}
