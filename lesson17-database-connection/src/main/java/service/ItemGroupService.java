1246c1016b37048bc8bafc46872310cac593965e
dquoc <ducquocvyy@gmail.com>
2025-01-12 13:36:36 +0700

    lesson17-database-connection 12.01.2025

---
+++ lesson17-database-connection/src/main/java/service/ItemGroupService.java
@@ -0,0 +1,72 @@
+package service;
+
+import java.util.List;
+
+import persistence.ItemGroup;
+
+public interface ItemGroupService {
+	
+	/**
+	 * Get all item groups.
+	 * 
+	 * @return list of {@link ItemGroup}
+	 */
+	List<ItemGroup> getAll();
+	
+	/**
+	 * Get unique {@link ItemGroup} with given id
+	 * 
+	 * @param id the id
+	 * @return available {@link ItemGroup} or null if not exist
+	 */
+	ItemGroup get(Integer id);
+	
+	/**
+	 * Get unique {@link ItemGroup} with given name
+	 * 
+	 * @param name the name
+	 * @return available {@link ItemGroup} or null if not exist
+	 */
+	ItemGroup get(String name);
+	
+	/**
+	 * Save group to database
+	 * 
+	 * @param group the group 
+	 * throw SQLIntegrityConstraintViolationException if group name is overlapped
+	 * TODO: need validate before saving ...
+	 */
+	void save(ItemGroup group);
+	
+	/**
+	 * Save groups to database
+	 * 
+	 * @param group the group 
+	 * throw SQLIntegrityConstraintViolationException if group name is overlapped
+	 * TODO: need validate before saving ...
+	 */
+	void save(List<ItemGroup> groups);
+	
+	/**
+	 * Update item group
+	 * 
+	 * @param group the group 
+	 * throw Exception if group name is overlapped
+	 */
+	void update(ItemGroup group);
+	
+	
+	/**
+	 * Save or Update item group
+	 * 
+	 * @param group the group 
+	 */
+	void saveOrUpdate(ItemGroup group);
+	
+	/**
+	 * Merge item group
+	 * 
+	 * @param group the group 
+	 */
+	void merge(ItemGroup group);
+}
---
+++ lesson17-database-connection/src/main/java/view/Ex02ItemGroupView.java
@@ -0,0 +1,67 @@
+package view;
+
+import service.ItemGroupService;
+import service.ItemGroupServiceImpl;
+
+import static utils.PrintUtils.*;
+
+import java.util.List;
+
+import persistence.ItemGroup;
+
+public class Ex02ItemGroupView {
+	
+	private static ItemGroupService itemGroupService;
+	
+	static {
+		itemGroupService = new ItemGroupServiceImpl();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Bắt đầu chương trình");
+		System.out.println("==================\n");
+		
+		generate(
+			"1. Liệt kê tất cả các loại hàng", 
+			itemGroupService.getAll()
+		);
+		
+		generate(
+			"2. Liệt kê loại hàng có mã loại = 62",
+			itemGroupService.get(62)
+		);
+		
+		System.out.println("\n3. Thêm mới loại hàng\n");
+		itemGroupService.merge(new ItemGroup("Balo học sinh"));
+		
+		System.out.println("\n4. Cập nhật loại hàng\n");
+		itemGroupService.merge(new ItemGroup(1, "Áo cho em bé"));
+		
+		// sql injection
+		// escape: if letter matches ' "" ; * .... --> convert to unicode ...u...
+		//         make sure all passing text is normal letter
+		
+		generate(
+				"5. Liệt kê loại hàng có tên loại = Quần", 
+				itemGroupService.get("Quần")
+			);
+		
+		// Thêm N phần tử
+		// Nếu có 1/x phần tử lỗi --> các phần tử còn lại vẫn được thêm vào
+		// Yêu cầu: nếu tồn tại 1 loại hàng bị lỗi --> rollback toàn bộ dữ liệu về ban đầu
+		System.out.println("\n6. Thêm mới N loại hàng\n");
+		itemGroupService.save(
+			List.of(
+				new ItemGroup("Loại Hàng G8"),
+				new ItemGroup("Loại Hàng G9"),
+				new ItemGroup("Loại Hàng G10")
+			)
+		);
+		
+		System.out.println("\n==================");
+		System.out.println("Kết thúc chương trình");
+		
+
+	}
+
+}
